f6b9f23a4f3fc99e418e21011679f913e4c3afba
Archana <62561163+archanapunitha@users.noreply.github.com>
2020-05-16 18:44:02 +0530

    Bottom navigation bar commit

--- lib/HomePage.dart
+++ lib/HomePage.dart
@@ -6,10 +6,53 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  int _currentIndex=0;
+  final List<Widget> children=[];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+        title: Text("Bottom Navigation Demo",style: TextStyle(
+          color: Colors.white,
+          backgroundColor: Colors.deepPurpleAccent,
+          fontSize: 15.0,
+          fontWeight: FontWeight.bold
+        ),),
+      ),
+      body: children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTapTrapped,
+        currentIndex: _currentIndex,
+        items: [
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              title: Text('Home'),
+          ),
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.mail),
+              title: Text('Message'),
+          ),
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.person),
+              title: Text('Profile'),
+          ),
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.account_circle),
+              title: Text('Account'),
+          ),
+          new BottomNavigationBarItem(
+              icon: Icon(Icons.search),
+              title: Text('Search'),
+          ),
+
+        ],
+      ),
     );
+  }
+
+  void onTapTrapped(int Index) {
+    setState(() {
+      _currentIndex=Index;
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bottom_navigation/HomePage.dart';
 import'package:flutter/material.dart';
 
 void main(){
